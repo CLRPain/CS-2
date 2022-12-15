4de6b0e19d5caa29cbb57221e438eb1342ce5eae
Timnims <106060011+Timnims@users.noreply.github.com>
2022-12-15 15:04:56 -0800

    Add files via upload

--- timo/Mine Sweeper/minesweeper.py
+++ timo/Mine Sweeper/minesweeper.py
@@ -11,8 +11,7 @@ class Mine_Sweeper(Fl_Window):
 		
 		fl_color(FL_WHITE)
 		
-		self.flags=0
-		self.squares=0
+		self.flags=0 # Total flags used
 		
 		self.flag_list=[]
 		self.button_list=[]
@@ -24,11 +23,12 @@ class Mine_Sweeper(Fl_Window):
 		row=0
 		col=0
 		
-		self.bomb = Fl_PNG_Image('bomb.png')
+		self.bomb = Fl_JPEG_Image('bomb.jpg')
 		self.bomb = self.bomb.copy(50,50)
 		self.flag = Fl_PNG_Image('flag.png')
 		self.flag = self.flag.copy(50,50)
-			
+		
+		#Button Creation (2D List)
 		for row in range(10):
 			
 			temp = []
@@ -43,7 +43,7 @@ class Mine_Sweeper(Fl_Window):
 				
 				
 			
-			
+		#Bomb Creation
 		y = random.sample(range(10),10)
 		z = random.sample(range(10),10)
 
@@ -52,20 +52,22 @@ class Mine_Sweeper(Fl_Window):
 			self.bomb_list.append(self.button_list[y[x]][z[x]])
 			self.bomb_index.append(self.find_ind(self.bomb_list[-1]))
 		
-		
-		self.flag_box = Fl_Box(210,520,75,25, f'Flags used: {self.flags}/10')
+		#Number of flags used
+		self.flag_box = Fl_Box(210,520,100,25, f'Flags used: {self.flags}')
 		
 		self.end()
 		
+		self.resizable(self)
+		
 		self.start=time.time()
 		
-	def gameover(self):		
+	def gameover(self):	 #When the game is lost
 		for x in range(10):
 			self.bomb_list[x].image(self.bomb)
 			self.bomb_list[x].redraw()
 		fl_message("Game Over!")
 		
-	def find_ind(self, wid):
+	def find_ind(self, wid): # Finds where a widget is in the 2d Button list
 		for x in self.button_list:
 			
 			if wid in x:
@@ -74,77 +76,75 @@ class Mine_Sweeper(Fl_Window):
 				return self.button_list.index(x), i
 		
 		
-	def reveal(self, wid):
+	def reveal(self, wid): #When a button is clicked
 		
-		if Fl.event_button() == FL_RIGHT_MOUSE:
+		if Fl.event_button() == FL_RIGHT_MOUSE: #If right click
 			
 			self.flagged(wid)
 			return
 		
-		if wid in self.flag_list:
+		if wid in self.flag_list: #If its a flag
 			
 			return
 		
-		if wid in self.bomb_list:
+		if wid in self.bomb_list: #If a bomb
 			self.gameover()
 			return
 		
 		coord = self.find_ind(wid)
 		
-		self.num(coord[0], coord[1])
+		self.num(coord[0], coord[1]) #Sends to a recursive function to reveal more buttons if it can
 		
 		wid.value(1)
 		
-		if len(set(self.revealed_list)) == 90:
+		if len(set(self.revealed_list)) == 90: #Win check
 			
 			self.win()
 		
-	def flagged(self,wid):
+	def flagged(self,wid): #Flag function
 		
 			
-			if wid in self.flag_list:
+			if wid in self.flag_list: #If button is already a flag
 				
 				wid.image(None)
 				self.flag_list.remove(wid)
 				self.flags-= 1
 				wid.redraw()
 				
-				self.flag_box.label(f'Flags used: {self.flags}/10')
+				self.flag_box.label(f'Flags used: {self.flags}')
 				
 				return
 				
-			if self.flags != 10:
+			if wid.value() == 1: # If not already a flag
+				return
 				
-				if wid.value() == 1:
-					return
+			self.flag_list.append(wid)
+			wid.image(self.flag)
+			self.flags += 1 
+			wid.redraw()
 				
-				self.flag_list.append(wid)
-				wid.image(self.flag)
-				self.flags += 1
-				wid.redraw()
-				
-				self.flag_box.label(f'Flags used: {self.flags}/10')
+			self.flag_box.label(f'Flags used: {self.flags}')
 		
-	def num(self, row, col):
+	def num(self, row, col): #Recursive function for setting the number for a button and revealing more buttons
 		
 		bombs=0
 		
-		for x in range(-1,2):
+		for x in range(-1,2): # 1 Button around the selected button
 			
 			for y in range(-1,2):
 				
-				if 0<=(row+x)<10 and 0<=(col+y)<10:
+				if 0<=(row+x)<10 and 0<=(col+y)<10: #If its in a corner/edge
 					
 					if self.button_list[row][col] not in self.flag_list:
 						self.revealed_list.append((row,col))
 						self.button_list[row][col].value(1)
 					
-					if (row+x, col+y) in self.bomb_index:
+					if (row+x, col+y) in self.bomb_index: #Finds total bombs
 						
 						bombs+=1
 						
 		
-							
+		#Changes color of num			
 		
 		if bombs == 1:
 			
@@ -157,6 +157,7 @@ class Mine_Sweeper(Fl_Window):
 			color=(FL_RED)
 			
 		if bombs > 0:
+			
 			if self.button_list[row][col] not in self.flag_list:
 				self.button_list[row][col].label(str(bombs))
 				self.button_list[row][col].labelcolor(color)
@@ -167,11 +168,11 @@ class Mine_Sweeper(Fl_Window):
 						
 				if 0<=(row+x)<10 and 0<=(col+y)<10:
 							
-					if (row+x, col+y) not in self.revealed_list:
-						self.num(row+x, col+y)
+					if (row+x, col+y) not in self.revealed_list: #makes it so it does recurse infintely
+						self.num(row+x, col+y) #Recurse
 				
 					
-	def win(self):
+	def win(self): #Win function
 		timetaken = time.time() - self.start
 		timetaken= round(timetaken, 1)
 		
