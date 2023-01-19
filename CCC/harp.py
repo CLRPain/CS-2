cddbe6800d71d7315762fd26dbe807dda44d0abf
CLRPain <102267741+CLRPain@users.noreply.github.com>
2023-01-18 23:35:14 -0800

    ye

--- CCC/harp.py
+++ CCC/harp.py
@@ -1,23 +1,3 @@
-'''turns = input()+'A'
-srt = 0
-for x in range(1 , len(turns)):
-    bef = turns[x-1].isnumeric()
-    now = turns[x].isnumeric()
-    #print(turns[x-1], turns[x])
-    #print(bef, now)
-
-    if bef == True and now == False:
-        new = turns[srt:x]
-        for y in new:
-            if y == '-':
-                print(turns[srt:turns.index(y)],' loosen ', turns[turns.index(y)+1:x])
-                srt = x
-                break
-            elif y == '+':
-                print(turns[srt:turns.index(y)],' tighten ', turns[turns.index(y)+1:x])
-                srt = x
-                break'''
-            
 turns = input()+'A'
 srt = 0
 turnlist = []
@@ -29,4 +9,7 @@ for x in range(1 , len(turns)):
         srt = x
 
 for y in turnlist:
-    print(y.replace('-', ''))
+    if '-' in y:
+        print(y.replace('-', ' loosen '))
+    elif '+' in y:
+        print(y.replace('+', ' tighten '))
