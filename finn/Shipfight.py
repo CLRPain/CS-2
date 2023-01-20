bafa64bab9463db6de30b9766e311ae275208695
TheLemonStew <117777372+TheLemonStew@users.noreply.github.com>
2023-01-20 15:00:17 -0800

    Add files via upload

--- finn/Shipfight.py
+++ finn/Shipfight.py
@@ -10,7 +10,13 @@ if sys.argv[1] == "client":
     host = sys.argv[2]
     port = int(sys.argv[3])
     s.connect( (host, port) )
-    
+    """
+    while True:
+        pshot, con = s.recv(1024)
+        pshot.decode()
+        #if con.decode() == True:
+        print("Hi")
+    """
 if sys.argv[1] == "server":
     type = "Server"
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,11 +26,21 @@ if sys.argv[1] == "server":
     s.bind( (host, port) )
     s.listen(1)
     conn, addr = s.accept()
+    """
+    while True:
+        pshot, con = conn.recv(1024)
+        pshot.decode()
+        #if con.decode() == True:
+        print("Hi")
+    """
 
 
 class Home(Fl_Window):
+    abl = {}
     bl = {}
     sl = {}
+    sturn = True
+    hit = False
     boats = 5
     def __init__(self, w, h, label = None):
         Fl_Window.__init__(self, w, h, label)
@@ -34,6 +50,8 @@ class Home(Fl_Window):
         self.boatimg = boatimg.copy(75,75)
         hitimg = Fl_PNG_Image("hit.png")
         self.hitimg = hitimg.copy(75,75)
+        missimg = Fl_PNG_Image("miss.png")
+        self.missimg = missimg.copy(75,75)
         for y in range(5):
             for x in range(5):
                 but = Fl_Button(25 + 75*x,25 + 75*y,75,75)
@@ -41,12 +59,22 @@ class Home(Fl_Window):
                 but.callback(self.ship, coord)
                 but.image(blankimg)
                 self.bl[coord] = but
+        
+        for y in range(5):
+            for x in range(5):
+                but = Fl_Button(25 + 75*x,425 + 75*y,75,75)
+                coord = (x,y)
+                but.image(blankimg)
+                self.abl[coord] = but
                  
         for x in range(5):
             box = Fl_Box(0,25 + 75*x,25,75, chr(65+x))
         for x in range(5):
             box = Fl_Box(25 + 75*x,0,75,25, str(x + 1))
-            
+        for x in range(5):
+            box = Fl_Box(0,425 + 75*x,25,75, chr(65+x))
+        for x in range(5):
+            box = Fl_Box(25 + 75*x,400,75,25, str(x + 1))
         self.resizable(self)    
             
             
@@ -60,16 +88,35 @@ class Home(Fl_Window):
                 for y in range(5):
                     for x in range(5):
                         self.bl[x,y].deactivate()
-                #win2 = Attack(0,458,400,400, f"Water Shoot {type}: Attack")
-                win2 = Attack(Home)
-                win2.show()
-                Fl.run()
-                
-class Attack(Home):
-    
-    def __init__(self, x, y, w, h, label = None):
-        super().__init__(self, x, y, w, h, label)
+                        if type == "Server":
+                            conn.send('Ready'.encode())
+                            data = conn.recv(1024).decode()
+                            if data == 'Ready':
+                                for y in range(5):
+                                    for x in range(5):
+                                        coord = (x,y)
+                                        self.abl[coord].callback(self.shoot, coord)
+                                        
+                        else:
+                            data = s.recv(1024).decode()
+                            s.send('Ready'.encode())
+                            if data == 'Ready':
+                                for y in range(5):
+                                    for x in range(5):
+                                        coord = (x,y)
+                                        self.abl[coord].callback(self.shoot, coord)
 
+    def shoot(self, wid, coord):
+        if type == "Server" and self.sturn == True:
+            self.sturn = False
+            shot = coord
+            conn.sendall(shot.encode(), self.sturn.encode())
+        if type == "Client" and self.sturn == False:
+            self.sturn = True
+            shot = coord
+            s.sendall(shot.encode(), self.sturn.encode())
+
+        
 """
 self.sl[(0,0)].activate()
 self.sl[(0,0)].image(self.hitimg)
@@ -78,7 +125,6 @@ self.sl[(0,0)].deactivate()
 #print("hi")
 """    
 
-
-win = Home(400,400,f"Water Shoot {type}: Home")
+win = Home(400,800,f"Water Shoot {type}: Home")
 win.show()
 Fl.run()
