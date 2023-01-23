bbe3d3d7494b913230dd0e72ccf75718a692c720
Timnims <106060011+Timnims@users.noreply.github.com>
2023-01-22 23:20:14 -0800

    Add files via upload

--- timo/battleship.py
+++ timo/battleship.py
@@ -1,55 +1,35 @@
-#python3 battleship.py [type] [IP] [port]
+#python3 battleship.py[type][IP][port]
 #[type] = server OR client
 from fltk import *
-import socket
-import sys
-
-
-''' 
-#TCP Connection
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-#For the person connecting
-if sys.argv[1] == 'client':
-	host = sys.argv[2]
-	port = int(sys.argv[3])
-	s.connect((host, port))
-	
-#For the person hosting
-if sys.argv[1] == 'server':
-	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	host = sys.argv[2]
-	post = int(sys.argv[3])
-	s.bind((host, port))
-	s.listen(1)
-	conn, addr = s.accept()
-'''
+import socket, sys, pickle
 
 class Battle_Ship(Fl_Window):
-	def __init__(self, x, y, w, h, label="Battle_Ship"):
+	def __init__(self, x, y, w, h, label=sys.argv[1]):
 		Fl_Window.__init__(self, x, y, w, h, label)
 		self.begin()
-		
+
 		gridsize=5
 		self.ships=5
 		self.myship_list=[]
-		self.opponentship_list=[(0,0),(1,1),(2,2),(3,3),(4,4)]
+		self.opponentship_list=[]
 		self.hits=0
 		self.misses=0
 		self.squares_shot=[]
+		self.turn='client'
+		self.am_ready=False
 
 		self.blank = Fl_PNG_Image("blank.png")
 		self.hit = Fl_PNG_Image("hit.png")
 		self.miss = Fl_PNG_Image("miss.png")
 		self.ship = Fl_PNG_Image("ship.png")
-		
+
 		self.blank = self.blank.copy(100,100)
 		self.hit = self.hit.copy(100,100)
 		self.miss = self.miss.copy(100,100)
 		self.ship = self.ship.copy(100,100)
-		
-		# Players Grid
-		
+
+#Players Grid
+
 		self.my_grid=[]
 		for x in range(gridsize):
 			temp=[]
@@ -60,9 +40,9 @@ class Battle_Ship(Fl_Window):
 				button.box(FL_UP_BOX)
 				temp.append(button)
 			self.my_grid.append(temp)
-			
-		# Opponents Grid
-		
+
+#Opponents Grid
+
 		self.opponents_grid=[]
 		for x in range(gridsize):
 			temp=[]
@@ -73,108 +53,177 @@ class Battle_Ship(Fl_Window):
 				button.box(FL_UP_BOX)
 				temp.append(button)
 			self.opponents_grid.append(temp)
-			
-		#Game chat
-		self.chat_brow=Fl_Multi_Browser(50,560,500,150)
-		
-		self.chat_input=Fl_Input(50,720,500,40,"Chat")
-		self.chat_input.align(FL_ALIGN_LEFT)
-		
-		#Computer helper
-		self.console_brow=Fl_Multi_Browser(580, 560, 500, 150)
-		#self.console_brow.add(f'Connection established with {addr}')
-		
-		#Ready button
+
+#Ready button
 		self.ready_button=Fl_Button(580,720,200,40,'Ready!')
 		self.ready_button.callback(self.ready)
-		
+
 		self.end()
-			
+		self.resizable(self)
+		self.connect()
+		
+	def connect(self):
+		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.host = sys.argv[2]
+		self.port = int(sys.argv[3])
+
+		if sys.argv[1] == 'server':
+
+			self.s.bind((self.host, self.port))
+			self.s.listen(1)
+			Fl.add_fd(self.s.fileno(), self.acceptConnection)
+
+		elif sys.argv[1] == 'client':
+			self.s.connect((self.host, self.port))
+
+			self.fd=self.s.fileno()
+			print(self.fd)
+			Fl.add_fd( self.fd, self.receive_data)
+
+	def acceptConnection(self, fd):
+		self.conn, addr = self.s.accept()
+		self.fd=self.conn.fileno()
+		print(self.fd)
+		Fl.add_fd(self.fd, self.receive_data) 
+
+
 	def place_ships(self, w): #When you get to place your ships
-		
-		if w not in self.myship_list:
-			if self.ships < 1:
-				
-				return
-			self.myship_list.append(w)
-			self.ships = self.ships - 1
-			
-			w.image(self.ship)
-			w.redraw()
+		if self.am_ready == True:
 			return
-			
-		else:
-			self.myship_list.remove(w)
-			self.ships +=1
-			w.image(self.blank)
-			w.redraw()
-			
-		return
-		
-	def ready(self,w):
-		if self.ships == 0:
-			self.console_brow.add('Ready!')
-			self.ready_button.deactivate()
-		else:
-			self.console_brow.add('You need to place all ships to be ready!')
-			
-	def shoot(self, w): #When you shoot
-		#check if its my turn
-		for x in self.opponents_grid:
+		for x in self.my_grid:
 
 			if w in x:
 
 				i = x.index(w)
-				ind = (self.opponents_grid.index(x), i)
-		if ind in self.squares_shot:
+				ind = (self.my_grid.index(x), i)
+				
+				
+		if ind not in self.myship_list:
+			if self.ships < 1:
+
+				return
+			self.myship_list.append(ind)
+			self.ships = self.ships - 1
+
+			w.image(self.ship)
+			w.redraw()
 			return
-		self.squares_shot.append(ind)
-		
-		if ind in self.opponentship_list:
-			self.hit_ship(ind,'player')
+
 		else:
-			self.miss_ship(ind,'player')
-        
-        
-		
-		#send ind
-		#recive ind
-		#display
-		
+			self.myship_list.remove(ind)
+			self.ships +=1
+			w.image(self.blank)
+			w.redraw()
+
 		return
+
+	def ready(self,w):
+		if self.ships == 0:
+			self.ready_button.deactivate()
+			data=pickle.dumps(self.myship_list)
+			if sys.argv[1] == 'client':
+				print('client')
+				self.s.sendall(data)
+
+				
+			else:
+
+
+				
+				self.conn.sendall(data)
+				
+			self.opponentship_list = pickle.loads(data)	
+			self.am_ready = True
+			self.placed = True
+
+			
 		
-	def gameover(self,win): #When the game is won/lost
-		if win == 'win':
-			fl_message(f'Congrats you won!\n You shot {self.misses+self.hits} missles!')
-		else:
-			fl_message(f'Oh no! You ran out of vessels, Better luck next time!\n You shot {self.misses+self.hits} missles!')
+			
+
+	def shoot(self, w): #When you shoot
+		if self.am_ready == False:
+			return
 		
+		if sys.argv[1] == self.turn:
+			for x in self.opponents_grid:
+
+				if w in x:
+
+					i = x.index(w)
+					
+					ind = (self.opponents_grid.index(x), i)
+					
+					print('ind',ind)
+			if ind in self.squares_shot:
+				return
+				
+			
+			
+			if ind in self.opponentship_list:
+				self.hit_ship(ind,'player')
+			else:
+				self.miss_ship(ind,'player')
+			print('hi')
+			data = pickle.dumps(ind)
+			if sys.argv[1] == 'client':
+				self.s.sendall(data)
+			elif sys.argv[1] == 'server':
+				self.conn.sendall(data)
+			
+			self.squares_shot.append(ind)
+			
+			if self.turn == 'client':
+				self.turn = 'server'
+			else:
+				self.turn = 'client'
+
+#data = self.conn.recv(1024)
+		
+
+
 	def miss_ship(self, ind, player): #When you miss
 		if player == 'player':
 			self.opponents_grid[ind[0]][ind[1]].image(self.miss)
 			self.misses += 1
+			self.opponents_grid[ind[0]][ind[1]].redraw()
 		else:
 			self.my_grid[ind[0]][ind[1]].image(self.miss)
-		
+			self.opponents_grid[ind[0]][ind[1]].redraw()
+			
 	def hit_ship(self, ind, player): #When you hit a ship
 		if player == 'player':
 			self.opponents_grid[ind[0]][ind[1]].image(self.hit)
+			self.opponents_grid[ind[0]][ind[1]].redraw()
 			self.hits+=1
 			if self.hits == 5:
 				self.gameover('win')
 		else:
 			self.my_grid[ind[0]][ind[1]].image(self.hit)
-			
-	def chat(self): #Chat function
-		return
+			self.my_grid[ind[0]][ind[1]].redraw()
+
+	def receive_data(self, fd): #When you receive data
+		if sys.argv[1] == 'server':
+			data = self.conn.recv(1024)
+		elif sys.argv[1] == 'client':
+			data = self.s.recv(1024)
+		data = pickle.loads(data)
+		print('data:',data)
+
+
+	def gameover(self,win): #When the game is won/lost
+		for x in range(len(opponents_grid)):
+			for y in range(len(opponents_grid[x])):
+				opponents_grid[x][y].deactivate()
+		for x in range(len(my_grid)):
+			for y in range(len(my_grid[x])):
+				my_grid[x][y].deactivate()
+		if win == 'win':
+			fl_message(f'Congrats you won!\n You shot {self.misses+self.hits} missles!')
+		else:
+			fl_message(f'Oh no! You ran out of vessels, Better luck next time!\n You shot {self.misses+self.hits} missles!')
 
 size = 800
 app = Battle_Ship(0, 0, size+300, size)
 Fl.scheme('plastic')
 app.show()
 Fl.run()
-
-1
-#556755 556755 556754 456766 667877 54
-#`121`21232131232132343243454354575475787868986978987989098090-09
-
